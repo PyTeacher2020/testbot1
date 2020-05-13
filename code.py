7441141f199ea90c1a3a120305f26e10b0a2b2b5
Veliade <veliade.saliieva@nure.ua>
2020-05-13 15:17:05 +0300

    Initial Commit

--- code.py
+++ code.py
@@ -2,7 +2,7 @@ import telebot
 import parser
 
 #main variables
-
+TOKEN = "1163060645:AAFJCM4Ud8ytSwYAjQVi4TS8Oi04hBMbxp4"
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'go'])
